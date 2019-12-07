ed335c2c92755013f32bd02d22b584f60350db11
dagrishin <grishin-dim@yandex.ru>
2019-12-07 15:52:31 +0500

    Lesson_7 3 задание, убрал дублирующие проверки, ускорил в 50 раз этим алгоритм

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -23,40 +23,43 @@ def median_sort(array):
 def find_median(array):
     index = 0
     min = len(array)
+    unique_dict = {}
     for i in range(len(array)):
-        count_right = 0
-        count_left = 0
-        count = 0
-        for j in range(len(array)):
-            if i != j:
-                if array[i] < array[j]:
-                    count_right += 1
-                elif array[i] > array[j]:
-                    count_left += 1
-                else:
-                    count += 1
-        # print(array[i], count_right, count_left, count)
-        if count_left == len(array) // 2 or count_right == len(array) // 2 or count == len(
-                array) or count_left == count_right or abs(count_left - count_right) == count:
-            return array[i]
-        if min > abs(abs(count_left - count_right) - count):
-            min = abs(abs(count_left - count_right) - count)
-            index = i
+        if not unique_dict.setdefault(array[i]):
+            unique_dict[array[i]] = i
+            count_right = 0
+            count_left = 0
+            count = 0
+            for j in range(len(array)):
+                if i != j:
+                    if array[i] < array[j]:
+                        count_right += 1
+                    elif array[i] > array[j]:
+                        count_left += 1
+                    else:
+                        count += 1
+            # print(array[i], count_right, count_left, count)
+            if count_left == len(array) // 2 or count_right == len(array) // 2 or count == len(
+                    array) or count_left == count_right or abs(count_left - count_right) == count:
+                return array[i]
+            if min > abs(abs(count_left - count_right) - count):
+                min = abs(abs(count_left - count_right) - count)
+                index = i
     return array[index]
 
 
 # array = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 9]
 
 
-for i in range(100000):
-    array = [random.randint(0, 9) for i in range(n)]
-    med_1 = median_sort(array)
-    med_2 = find_median(array)
-    # print('№', i, med_1, med_2)
-    if med_1 != med_2:
-        print('№', i, med_1, med_2)
-        print(array.sort())
-        break
+# for i in range(10000):
+#     array = [random.randint(0, 9) for i in range(n)]
+#     med_1 = median_sort(array)
+#     med_2 = find_median(array)
+#     # print('№', i, med_1, med_2)
+#     if med_1 != med_2:
+#         print('№', i, med_1, med_2)
+#         print(array.sort())
+#         break
 print(
     timeit.timeit(
         "median_sort(array)",
@@ -68,5 +71,6 @@ print(
         setup="from __main__ import find_median, array",
         number=1000))
 # нахождение медианы по отсортированному массиву рвет в клочья мой алгоритм поиска медианы, хотя мой алгоритм и рабочий
+# но он в 100 раз медленее
 # median_sort 0.0034864000000000006
-# find_median 1.4514715
+# find_median 0.3
